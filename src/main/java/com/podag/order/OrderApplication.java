521d1a937ec46a6c3aa5ce92a8b6a3bbb30f5bf8
SurikovKirill <kirill98s@bk.ru>
2019-10-19 11:31:55 +0300

    final touches [3]

--- src/main/java/com/podag/order/OrderApplication.java
+++ src/main/java/com/podag/order/OrderApplication.java
@@ -1,13 +1,17 @@
 package com.podag.order;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderApplication {
+    private static final Logger LOGGER= LoggerFactory.getLogger(OrderApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+        LOGGER.info("Starting programm");
     }
 
 }
