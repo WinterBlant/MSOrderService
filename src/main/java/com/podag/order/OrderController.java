f172d6d20e31933fe5f2e4d27ad45e96848d9edf
SurikovKirill <kirill98s@bk.ru>
2019-10-19 11:56:27 +0300

    final touches [4]

--- src/main/java/com/podag/order/OrderController.java
+++ src/main/java/com/podag/order/OrderController.java
@@ -6,6 +6,8 @@ import com.podag.order.Service.OrderService;
 import com.podag.order.dto.ItemAdditionParametersDTO;
 import com.podag.order.dto.OrderDTO;
 import com.podag.order.entity.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +21,19 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderService ordserv;
 
     @GetMapping
     public ResponseEntity<List<Order>> getOrders() {
         try {
+            LOGGER.info("Calling method getOrders");
             return ResponseEntity.ok().body(ordserv.findAll());
         }
         catch (InvalidSearchFilterException e) {
+            LOGGER.warn("No orders in the database");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e){
             e.printStackTrace();
@@ -38,8 +44,10 @@ public class OrderController {
     @GetMapping("{orderid}")
     public ResponseEntity<OrderDTO> getOrderById(@PathVariable(value = "orderid") Integer orderID) {
         try {
+            LOGGER.info("Calling method getOrderById with orderID = {}", orderID);
             return ResponseEntity.ok().body(ordserv.findByID(orderID));
         } catch (InvalidParameterException e){
+            LOGGER.error("No order with such ID found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,6 +60,7 @@ public class OrderController {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             ItemAdditionParametersDTO itempar = objectMapper.readValue(data, ItemAdditionParametersDTO.class);
+            LOGGER.info("Calling method addToOrder with orderID = {} and item with name = {} and price = {}", orderID, itempar.getName(), itempar.getPrice());
             return ResponseEntity.ok().body(ordserv.addItem(orderID, itempar));
         } catch (Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -61,9 +70,16 @@ public class OrderController {
     @RequestMapping(value = "{orderid}/status/{status}", method = RequestMethod.PUT)
     public ResponseEntity<OrderDTO> changeOrderStatus (@PathVariable(value = "orderid") Integer orderID, @PathVariable(value = "status") String status) {
         try{
+            LOGGER.info("Calling method changeOrderStatus with orderID = {} and status = {}", orderID, status);
             return ResponseEntity.ok().body(ordserv.changeStatus(orderID, status));
         } catch (InvalidParameterException e){
+            LOGGER.error("No order with such ID exists");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("No status with such status name exists");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 }
